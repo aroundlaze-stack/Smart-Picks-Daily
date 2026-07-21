bb35143e44d514a88d9093dd65160dc9fcb877de
aroundlaze-stack <aroundlaze@gmail.com>
2026-07-21 13:25:16 +0530

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,5 @@
 import { initializeAnalytics } from "./lib/analytics";
-import { initializeClarity } from "./lib/clarity";
+/* import { initializeClarity } from "./lib/clarity"; */
 import { createRoot } from 'react-dom/client';
 
 import App from './App';
@@ -7,5 +7,5 @@ import App from './App';
 import './index.css';
 
 initializeAnalytics();
-initializeClarity();
+/*initializeClarity(); */
 createRoot(document.getElementById('root')!).render(<App />);
