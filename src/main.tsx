d19aff9ee33811e2ce5e830578bf592874c4376b
aroundlaze-stack <aroundlaze@gmail.com>
2026-07-21 12:43:20 +0530

    Add files via upload

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,10 @@
+import { initializeAnalytics } from "./lib/analytics";
 import { createRoot } from 'react-dom/client';
 
 import App from './App';
 
 import './index.css';
+
+initializeAnalytics();
 
 createRoot(document.getElementById('root')!).render(<App />);
