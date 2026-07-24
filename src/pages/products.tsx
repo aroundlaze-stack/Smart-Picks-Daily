1ecab53bfdf4596d265932680c6dc8fa68c8c396
aroundlaze-stack <aroundlaze@gmail.com>
2026-07-24 20:37:37 +0530

    Add files via upload

--- src/pages/products.tsx
+++ src/pages/products.tsx
@@ -504,7 +504,7 @@ export default function Products() {
           </div>
 
           {/* Grid */}
-          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6">
+          <div className="grid grid-cols-2 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-3 sm:gap-6">
             <AnimatePresence mode="popLayout" initial={false}>
               {visibleProducts.map((product, i) => (
                 <motion.div
